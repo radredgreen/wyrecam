92977236558506ce6a54b3a4abac0db81dfabb2d
radredgreen <radredgreen>
2024-02-18 10:40:36 -0600

    Add a mavlink build option

--- general/package/mavlink-router/mavlink-router.mk
+++ general/package/mavlink-router/mavlink-router.mk
@@ -1,8 +1,14 @@
 
 #POSITRON_WYRECAM_LICENSE = GPL v3
 
-MAVLINK_ROUTER_SITE_METHOD = local
-MAVLINK_ROUTER_SITE = ../../mavlink-router-cmake
+#MAVLINK_ROUTER_SITE_METHOD = local
+#MAVLINK_ROUTER_SITE = ../../mavlink-router-cmake
+
+MAVLINK_ROUTER_VERSION = 8c4c670cdb2c1761bb538bc34cbf8bbb942ceada
+MAVLINK_ROUTER_SITE_METHOD = git
+MAVLINK_ROUTER_SITE = https://github.com/radredgreen/mavlink-router-cmake.git
+#MAVLINK_ROUTER_GIT_SUBMODULES = YES
+
 
 $(eval $(cmake-package))
 
